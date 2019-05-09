366ca33b479399f705a8fc9b7ea1a6d4f93151f0
matthew <yyygggfff@hotmail.com>
2019-05-09 20:48:11 +0100

    test with mt only for now

--- model_interference_test.py
+++ model_interference_test.py
@@ -253,25 +253,8 @@ def run(
 def main():
     # which one we should run in parallel
     sets = [
-            ['googlenet_cmd'],
-            ['mobilenetv2_cmd'],
-            ['vgg19_cmd'],
-            ['pos_cmd'],
             ['mt1_cmd'],
             ['mt2_cmd'],
-            ['googlenet_cmd','googlenet_cmd'],
-            ['mobilenetv2_cmd','mobilenetv2_cmd'],
-            ['vgg19_cmd', 'vgg19_cmd'],
-            ['pos_cmd', 'pos_cmd'],
-            ['mt1_cmd','mt1_cmd'],
-            ['googlenet_cmd', 'mobilenetv2_cmd'],
-            ['googlenet_cmd', 'vgg19_cmd'],
-            ['googlenet_cmd', 'pos_cmd'],
-            ['googlenet_cmd', 'mt1_cmd'],
-            ['mobilenetv2_cmd', 'vgg19_cmd'],
-            ['mobilenetv2_cmd', 'pos_cmd'],
-            ['mobilenetv2_cmd', 'mt1_cmd'],
-	          ['mobilenetv2_cmd', 'mt2_cmd'] 
            ]
     project_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     experiment_path = os.path.join(project_dir, 'experiment')
