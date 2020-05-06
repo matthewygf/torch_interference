257c538478348191f896d289beb7395df84a2ca3
Matthew Yeung <yyygggfff@hotmail.com>
2020-05-06 11:28:53 +0800

    WIP - remove extra space

--- image_models/factory.py
+++ image_models/factory.py
@@ -57,7 +57,6 @@ def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
-
 class Bottleneck(nn.Module):
     expansion = 4
 
